4767531029ffe39704f504e4899c114be3cdec9c
Kip <88916382+UrbaneD0ge@users.noreply.github.com>
2021-11-19 18:05:49 -0500

    import Dev foleder, building server folder

---
+++ server/schemas/index.js
@@ -0,0 +1,4 @@
+const typeDefs = require('./typeDefs');
+cont resolvers = require('./resolvers');
+
+module.exports = {typeDefs, resolvers};
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+const {gql} = require{'apollo-server-express'};
+
+const typeDefs = gql`
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+}
+type Book {
+    bookID: String
+    title: String
+    image: String
+    link: String
+}
+type Auth {
+    token: ID!
+    user: User
+}
+input BookInput {
+    bookID: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+type Query {
+    me: User
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String, email: String!, password: savedBook(bookData: BookInput!): User
+    removeBook(bookID: String!): User
+}
+`;
+
+module.exports = typeDefs;
