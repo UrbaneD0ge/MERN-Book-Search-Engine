7ac775f6a867d443638101fc0fea5bb5a01cf93d
Kip <kipling.dunlap@gmail.com>
2021-11-20 21:58:39 -0500

    why you no?

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
-const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
@@ -11,8 +10,7 @@ const app = express();
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
-  context: authMiddleware
+  resolvers
 });
 
 server.applyMiddleware({ app });
@@ -25,7 +23,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
